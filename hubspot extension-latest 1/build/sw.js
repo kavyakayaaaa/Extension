1ca5390a6dcbcd2cf39eb0d5331ff0d81ff88d1b
Kavya N <154525619+kavyakayaaaa@users.noreply.github.com>
2024-04-03 18:19:46 +0530

    Update sw.js

--- hubspot extension-latest 1/build/sw.js
+++ hubspot extension-latest 1/build/sw.js
@@ -1,6 +1,4 @@
-  // await chrome.storage.session.set({'windVal':'window'});
-    // await chrome.action.setPopup({popup: '../popup1.html'});
-    //  await chrome.action.openPopup();
+
     chrome.action.onClicked.addListener(async t => {
         // await chrome.storage.session.set({'windVal':'window'});
         // await chrome.action.setPopup({popup: '../popup1.html'});
